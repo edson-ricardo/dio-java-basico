f3362b8167bb112be4057f077010cf3b5309ca7a
Edson Santos <nosderico@gmail.com>
2023-10-25 20:31:56 -0300

    desafio concluído

--- desafio-codigo/conta-banco/src/ContaTerminal.java
+++ desafio-codigo/conta-banco/src/ContaTerminal.java
@@ -9,21 +9,21 @@ public class ContaTerminal {
         //String agencia = "067-8";        
         Double saldo = 237.48;
         
-        System.out.println("Digite o seu Nome: ");
+        System.out.print("Digite o seu Nome: ");
         String nomeCliente = scanner.nextLine();        
 
-        System.out.println("Digite o Número da sua Agência: ");
+        System.out.print("Digite o Número da sua Agência: ");
         String agencia = scanner.next();
         scanner.nextLine();
         
-        System.out.println("Digite o Número da sua Conta: ");
+        System.out.print("Digite o Número da sua Conta: ");
         int numeroConta = scanner.nextInt();
         scanner.nextLine();        
 
         //TODO: Exibir a mensagem conta criada
         System.out.println("-----------------------------------------------------");
-        System.out.println("Olá, " + nomeCliente + ".\nObrigado por criar uma conta em nosso banco. \nSua agência é: "
-        + agencia + ". \nSua conta é: " + numeroConta + ".\nO seu saldo de " + saldo + " já está disponível para saque.");
+        System.out.println("Olá, " + nomeCliente + ".\nObrigado por criar uma conta em nosso banco. \nSua Agência é: "
+        + agencia + ". \nSua Conta é: " + numeroConta + ".\nO seu saldo de " + saldo + " já está disponível para saque.");
         System.out.println("-----------------------------------------------------");
     }
 }
